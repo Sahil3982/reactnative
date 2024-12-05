f89d215757ba06e2fd0a85e7cbeeecffb96e426f
Sahil3982 <sahilpatel3982@gmail.com>
2024-12-05 23:44:54 +0530

    Customize index.js file

---
+++ HelloWorld/app/(tabs)/setting.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from 'react-native'
+
+const Setting = () => {
+  return (
+    <SafeAreaView>
+        <View>
+        <Text>Setting</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default Setting
